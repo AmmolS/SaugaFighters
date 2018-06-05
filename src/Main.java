86daa3b3fdbe4d4710dde1997869b0c7db37a61b
Faiz Patel <adv.faizp@gmail.com>
2018-06-05 19:57:10 -0400

    Kratos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.security.KeyRep;
+
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -7,41 +9,31 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
-public class Main    extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws FileNotFoundException {
-        //Creating an image
-        Image image = new Image
-                (new FileInputStream("C:\\Users\\user\\Desktop\\CSproject\\StreetFighters\\src\\Assets\\Background1.gif"));
 
-        //Setting the image view
-        ImageView imageView = new ImageView(image);
-
-        //Setting the position of the image
+        Image background = new Image(new FileInputStream
+                ("C:\\Users\\user\\Desktop\\CSproject\\StreetFighters\\src\\Assets\\Background1.gif"));
+        ImageView imageView = new ImageView(background);
         imageView.setX(0);
         imageView.setY(0);
-
-        //setting the fit height and width of the image view
-        imageView.setFitHeight(603);
-        imageView.setFitWidth(1200);
-
-        //Setting the preserve ratio of the image view
-        imageView.setPreserveRatio(true);
-
-        //Creating a Group object
         Group root = new Group(imageView);
-
-        //Creating a scene object
         Scene scene = new Scene(root, 1200, 603);
-
-        //Setting title to the Stage
         stage.setTitle("Street Fighter");
-
-        //Adding scene to the stage
         stage.setScene(scene);
-
-        //Displaying the contents of the stage
         stage.show();
+
+        Image image11 = new Image(new FileInputStream
+                ("C:\\Users\\user\\Desktop\\CSproject\\StreetFighters\\src\\Assets\\Kratos\\KratosStance.gif"));
+        ImageView Kratos = new ImageView();
+        Kratos.setFitHeight(300);
+        Kratos.setFitWidth(300);
+        Kratos.setImage(image11);
+        Kratos.setPreserveRatio(true);
+        Kratos.setX(300);
+        Kratos.setY(300);
+        root.getChildren().add(Kratos);
     }
     public static void main(String args[]) {
         launch(args);
