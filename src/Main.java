85d7a0e22e3fd5a845c79144fb591fa3930ac6db
Faiz Patel <adv.faizp@gmail.com>
2018-06-05 19:30:55 -0400

    Kratos

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,49 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
-import java.awt.*;
+public class Main    extends Application {
+    @Override
+    public void start(Stage stage) throws FileNotFoundException {
+        //Creating an image
+        Image image = new Image
+                (new FileInputStream("C:\\Users\\user\\Desktop\\CSproject\\StreetFighters\\src\\Assets\\Background1.gif"));
 
-public class Main extends Application {
+        //Setting the image view
+        ImageView imageView = new ImageView(image);
 
+        //Setting the position of the image
+        imageView.setX(0);
+        imageView.setY(0);
 
-    public static void main(String[] args)
-    {
+        //setting the fit height and width of the image view
+        imageView.setFitHeight(603);
+        imageView.setFitWidth(1200);
+
+        //Setting the preserve ratio of the image view
+        imageView.setPreserveRatio(true);
+
+        //Creating a Group object
+        Group root = new Group(imageView);
+
+        //Creating a scene object
+        Scene scene = new Scene(root, 1200, 603);
+
+        //Setting title to the Stage
+        stage.setTitle("Street Fighter");
+
+        //Adding scene to the stage
+        stage.setScene(scene);
+
+        //Displaying the contents of the stage
+        stage.show();
+    }
+    public static void main(String args[]) {
         launch(args);
     }
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Street Fighters");
-        StackPane layout = new StackPane();
-        Scene scene = new Scene(layout, 1200, 603);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
 }
