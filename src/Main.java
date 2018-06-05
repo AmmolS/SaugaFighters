725b4fd90bb50baeb0f6d615e9177f6e65484d0a
Faiz Patel <adv.faizp@gmail.com>
2018-06-05 16:56:59 -0400

    Kratos

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,26 @@
-public class Main
-{
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.awt.*;
+
+public class Main extends Application {
+
+
     public static void main(String[] args)
     {
-        System.out.println();
+        launch(args);
     }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Street Fighters");
+        StackPane layout = new StackPane();
+        Scene scene = new Scene(layout, 1200, 603);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
 }
