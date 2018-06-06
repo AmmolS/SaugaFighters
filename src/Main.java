7249a4b9c66e2ed9249600ff33f0c114a24ca84d
Faiz Patel <adv.faizp@gmail.com>
2018-06-06 17:54:24 -0400

    Kratos

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
+import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.net.URL;
 import java.security.KeyRep;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
+
 
 public class Main extends Application {
     @Override
@@ -26,15 +32,38 @@ public class Main extends Application {
         Image image11 = new Image("/Assets/Kratos/KratosStance.gif");
         ImageView Kratos = new ImageView();
         Kratos.setFitHeight(300);
-        Kratos.setFitWidth(300);
+        Kratos.setFitWidth(290);
         Kratos.setImage(image11);
         Kratos.setPreserveRatio(true);
         Kratos.setX(300);
         Kratos.setY(300);
         root.getChildren().add(Kratos);
+
+        scene.setOnKeyPressed(e->{
+            //while (Kratos.getLayoutX() > 0)
+            //{
+                if (e.getCode() == KeyCode.RIGHT) {
+                    //change the value of x and y appropriately
+                    Kratos.setLayoutX(Kratos.getLayoutX() + 10);
+                } else if (e.getCode() == KeyCode.LEFT) {
+                    //change the value of x and y appropriately
+                    Kratos.setLayoutX(Kratos.getLayoutX() - 10);
+                }
+            //}
+            //check for other arrow keys
+        });
+
+
+
+
+        stage.getIcons().add(new Image("/Assets/Logo.png"));
     }
+
+
+
     public static void main(String args[]) {
         launch(args);
         System.out.println("");
     }
+
 }
