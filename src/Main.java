32722a82091b9609e820a1cc7c96591ff8ba953a
Faiz Patel <adv.faizp@gmail.com>
2018-06-07 12:44:07 -0400

    Kratos

--- src/Main.java
+++ src/Main.java
@@ -86,6 +86,7 @@ public class Main extends Application {
     public static void main(String args[]) {
         launch(args);
         System.out.println("");
+        System.out.println();
     }
 
 }
