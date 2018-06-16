672257273afaa119a5e6511cd968d3fb3f54e0e4
cp2701 <725170@pdsb.net>
2018-06-16 00:02:07 -0400

    Music

---
+++ src/UserInterface/kick.java
@@ -0,0 +1,9 @@
+package UserInterface;
+
+import javafx.scene.media.Media;
+
+public class kick {
+    public kick(){
+        Media punch = new Media("/UserInterface/Effects/kick.wav");
+    }
+}
---
+++ src/UserInterface/punch.java
@@ -0,0 +1,9 @@
+package UserInterface;
+
+import javafx.scene.media.Media;
+
+public class punch {
+    public punch(){
+        Media punch = new Media("/UserInterface/Effects/punch.wav");
+    }
+}
